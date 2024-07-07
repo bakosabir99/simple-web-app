953b29fb94e4cbc461a5ea5214bdd06eef580c9b
bako sabir <122810133+bako-sabir@users.noreply.github.com>
2024-07-07 16:09:05 +0300

    Initial setup for unique version of web app

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,14 @@
-# Use an official Python runtime as a parent image
+# Dockerfile for the unique version of the web app
 FROM python:3.8-slim
 
-# Set the working directory
 WORKDIR /app
 
-# Copy the current directory contents into the container at /app
 COPY . /app
 
-# Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Make port 80 available to the world outside this container
 EXPOSE 80
 
-# Define environment variable
-ENV NAME World
+ENV NAME UniqueVersion
 
-# Run app.py when the container launches
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,33 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-# Sample data for to-do items
-todos = [
-    {"id": 1, "task": "Learn Flask", "done": False},
-    {"id": 2, "task": "Build a CI/CD pipeline", "done": False}
-]
-
 @app.route('/')
-def home():
-    return "Welcome to the To-Do List App!"
-
-@app.route('/todos', methods=['GET'])
-def get_todos():
-    return jsonify(todos)
-
-@app.route('/todos', methods=['POST'])
-def add_todo():
-    new_todo = request.json
-    new_todo["id"] = len(todos) + 1
-    todos.append(new_todo)
-    return jsonify(new_todo), 201
-
-@app.route('/todos/<int:todo_id>', methods=['DELETE'])
-def delete_todo(todo_id):
-    global todos
-    todos = [todo for todo in todos if todo["id"] != todo_id]
-    return '', 204
+def hello():
+    return "Hello, this is a unique version of the web app!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0')
